3f0618cb35c7faf6e70da1be0b337919f07df43e
marina.azzi011 <marina.azzi011@gmail.com>
2020-09-30 17:03:21 -0300

    início dos exercícios sobre condicionais

---
+++ semana3/condicionais/js-vanilla-template/src/index.js
@@ -0,0 +1,71 @@
+// EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO
+// Exercício 1
+
+// O código mostrado no exercício serve para testar se o número inserido pelo usuário é par ou ímpar.
+// Se o resto da divisão por 2 for 0, o número é par (mensagem Passou no teste.)
+// Se o resto da divisão por 2 for 1, o número é ímpar (mensagem Não passou no teste.)
+
+// Exercício 2
+
+// a) O código do exercício 2 serve para mostrar o preço de determinadas frutas.
+// b) A mensagem impressa será "O preço da fruta maçã é RS2.25."
+// c) A mensagem seria de erro.
+
+// Exercício 3
+
+// a) A primeira linha está declarando a variável numero e mandando uma mensagem para usuários, pedindo que a pessoa digite o primeiro número.
+// b) Se a pessoa digitar o número 10, a mensagem será "Esse número passou no teste". Se o número digitado fosse -10, não haveria nenhuma mensagem no terminal.
+// c) Sim, pois não há uma condição declarada para valores menores ou iguais a 0.
+
+// EXERCÍCIOS DE ESCRITA DE CÓDIGO
+// Exercício 4
+
+// const idade = Number(prompt("Qual sua idade?"))
+
+// if (idade >= 18) {
+//     console.log("Você pode dirigir.")
+// } else {
+//     console.log("Você não pode dirigir.")
+// }
+
+
+// Exercício 5
+
+// const turno = prompt("Em qual turno você estuda? Digite M para matutino, V para vespertino ou N para noturno.").toLowerCase()
+
+// if (turno === "m") {
+//     console.log("Bom dia!")
+// } else if (turno === "v") {
+//     console.log("Boa tarde!")
+// } else if (turno === "n") {
+//     console.log("Boa noite!")
+// } else {
+//     console.log("Oier! (:")
+// }
+
+
+// Exercício 6
+
+// const turno = prompt("Em qual turno você estuda? Digite M para matutino, V para vespertino ou N para noturno.").toLowerCase()
+
+// switch (turno) {
+//     case "m":
+//         console.log("Bom dia!")
+//         break
+//     case "v":
+//         console.log("Boa tarde!")
+//         break
+//     case "n":
+//         console.log("Boa noite!")
+//         break
+//     default:
+//         console.log("Oier! (:")            
+// }
+
+
+// Exercício 7
+
+
+
+
+
