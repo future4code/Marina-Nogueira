ba72c61b591716b906d6374a4628478c3e82bc39
marina.azzi011 <marina.azzi011@gmail.com>
2020-10-09 13:14:45 -0300

    INTERPRETAÇÃO: exercício 2 concluído

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -17,3 +17,35 @@
 
 //                   ------------//--------------
 
+// EXERCÍCIO 2
+
+// O código é iniciado com a declaração da função investeDinheiro,
+// cujos parâmetros são tipoDeInvestimento e valor.
+// Dentro dessa função são criadas a variável valorAposInvestimento
+// e o switch case que vai categorizar pelo tipoDeInvestimento.
+// O primeiro case é "Poupança" e dentro dele a variável valorAposInvestimento
+// vai resultar na multiplicação de valor por 1.03.
+// O segundo case é "Renda Fixa" e dentro dele a variável valorAposInvestimento
+// vai resultar na multiplicação de valor por 1.05.
+// O terceiro case é "CDB" e dentro dele a variável valorAposInvestimento
+// vai resultar na multiplicação de valor por 1.06.
+// O quarto case é "Ações" e dentro dele a variável valorAposInvestimento
+// vai resultar na multiplicação de valor por 1.1.
+// Por último, o switch case chama o default, que vai imprimir o alerta
+// "TIPO DE INVESTIMENTO INFORMADO INCORRETO" caso o parâmetro tipoDeInvestimento
+// seja preenchido com outra possibilidade que não Poupança, Renda Fixa, CDB ou Ações.
+// Após o final do switch case, a função chama o return, que vai informar o valor
+// da variável valorAposInvestimento.
+// A função é fechada e o código continua com a criação de duas variáveis:
+// novoMontante e segundoMontante.
+// A variável novoMontante é o resultado da função investeDinheiro com os parâmetros
+// "Ações" e 150.
+// A variável segundoMontante é o resultado da função investeDinheiro com os parâmetros
+// "Tesouro Direto" e 200.
+// Após isso, há dois console.log, um para imprimir a variável novoMontante e outro
+// para imprimir a variável segundoMontante.
+// O console.log(novoMontante) imprimiria na tela o valor 165.
+// Já o console.log(segundoMontante) resultaria no alert "TIPO DE INVESTIMENTO INFORMADO INCORRETO",
+// pois o parâmetro "Tesouro Direto" não existe no switch case da função.
+
+//                   ------------//--------------
