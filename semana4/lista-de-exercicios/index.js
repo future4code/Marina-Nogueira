c091c7c1e58c506147a9c85f7c927c8101c2a0d1
marina.azzi011 <marina.azzi011@gmail.com>
2020-10-09 13:14:01 -0300

    INTERPRETAÇÃO: exercício 1 concluído

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,19 @@
+// EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO
+
+// EXERCÍCIO 1
+
+// O código mostra uma função chamada conversorDeMoeda, 
+// cujo parâmetro é valorEmDolar.
+// Dentro dessa função é criada a variável cotacao, cujo valor 
+// vem da transformação em número do que é inserido no prompt
+// "Informe o valor da cotação em Dólar."
+// Depois disso, a função chama um return que vale a string "R$"
+// + o resultado de valorEmDolar*cotacao.
+// A condição é fechada e após ela, cria-se a variável meuDinheiro,
+// cujo valor é a função conversorDeMoeda com parâmetro 100.
+// O código é finalizado com o console.log, que irá imprimir na tela
+// o valor de meuDinheiro.
+// No caso, a mensagem impressa na tela seria R$18,10.
+
+//                   ------------//--------------
+
