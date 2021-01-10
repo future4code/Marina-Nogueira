4e0f017270431b6e01dde645f48200a3742cea06
Marina Azzi Nogueira <marinaazzinogueira@Marinas-MacBook-Pro.local>
2021-01-10 11:23:23 -0300

    validação de idade criada

--- semana15/projeto/src/accounts.ts
+++ semana15/projeto/src/accounts.ts
@@ -1,87 +1,15 @@
+export type transaction = {
+    value: number,
+    date: Date,
+    description: string
+}
+
 export type account = {
     name: string,
     cpf: number,
-    birthDate: string,
-    age: number,
+    birthDate: Date,
     balance: number,
     statement: transaction[]
 }
 
-export type transaction = {
-    value: number,
-    date: number,
-    description: string
-}
-
-export let accounts: account[] = [
-    {
-        "name": "Marina Azzi Nogueira",
-        "cpf": 38127167800,
-        "birthDate": "24/04/1990",
-        "age": 30,
-        "balance": 6000,
-        "statement": [
-            {
-                value: 100,
-                date: 1610281201,
-                description: "Internet Janeiro 2021"
-            }
-        ]
-    },
-    {
-        "name": "Fernanda",
-        "cpf": 10000000000,
-        "birthDate": "24/04/1990",
-        "age": 30,
-        "balance": 5000,
-        "statement": [
-            {
-                value: 100,
-                date: 1610281201,
-                description: "Internet Janeiro 2021"
-            }
-        ]
-    },
-    {
-        "name": "Diana",
-        "cpf": 20000000000,
-        "birthDate": "24/04/1990",
-        "age": 30,
-        "balance": 4000,
-        "statement": [
-            {
-                value: 100,
-                date: 1610281201,
-                description: "Internet Janeiro 2021"
-            }
-        ]
-    },
-    {
-        "name": "Augusto",
-        "cpf": 30000000000,
-        "birthDate": "24/04/1990",
-        "age": 30,
-        "balance": 3000,
-        "statement": [
-            {
-                value: 100,
-                date: 1610281201,
-                description: "Internet Janeiro 2021"
-            }
-        ]
-    },
-    {
-        "name": "Wesley",
-        "cpf": 40000000000,
-        "birthDate": "24/04/1990",
-        "age": 30,
-        "balance": 2000,
-        "statement": [
-            {
-                value: 100,
-                date: 1610281201,
-                description: "Internet Janeiro 2021"
-            }
-        ]
-    }
-]
+export let accounts: account[] = []
--- semana15/projeto/src/index.ts
+++ semana15/projeto/src/index.ts
@@ -1,6 +1,6 @@
 import express, {Express, Request, Response} from 'express'
 import cors from 'cors'
-import {account, transaction, accounts} from './accounts'
+import {account, accounts} from './accounts'
 
 const app: Express = express()
 
@@ -8,10 +8,22 @@ app.use(express.json());
 app.use(cors())
 
 app.get("/accounts", (req: Request, res: Response) => {
-    const accountsList = accounts
-    res
-        .status(200)
-        .send(accountsList)
+
+    try {
+        if(!accounts.length){
+            res
+                .statusCode = 404
+                throw new Error("Nenhuma conta encontrada.")
+        }
+
+        res
+            .status(200)
+            .send(accounts)
+    } catch (error) {
+        res
+            .send({message: error.message})
+    }
+    
 })
 
 app.post("/accounts/create", (req: Request, res: Response) => {
@@ -19,25 +31,31 @@ app.post("/accounts/create", (req: Request, res: Response) => {
     let errorCode: number = 400
 
     try {
+        const { name, cpf, birthDateAsString} = req.body
 
-        const reqBody: account = {
-            name: req.body.name,
-            cpf: req.body.cpf,
-            birthDate: req.body.birthDate,
-            age: req.body.age,
-            balance: 0,
-            statement: []
+        const [day, month, year] = birthDateAsString.split("/")
+
+        const birthDate: Date = new Date(`${year}-${month}-${day}`)
+
+        const ageInMilisseconds: number = Date.now() - birthDate.getTime()
+
+        const ageInYears: number = ageInMilisseconds / 1000 / 60 / 60 / 24 / 365
+
+        if (ageInYears < 18){
+            res.statusCode = 406
+            throw new Error("Usuárie deve ser maior de 18 anos.")
         }
 
-        // if(!reqBody.name || !reqBody.cpf || !reqBody.birthDate) {
-        //     errorCode= 422
-        //     throw new Error("Por favor, preencha todos os campos.")
-        // }
-
-        accounts.push(reqBody)
+        accounts.push({
+            name,
+            cpf,
+            birthDate,
+            balance: 0,
+            statement: []
+        })
 
         res
-            .status(200)
+            .status(201)
             .send({message: "Conta criada com sucesso!"})
     } catch (error) {
         res
